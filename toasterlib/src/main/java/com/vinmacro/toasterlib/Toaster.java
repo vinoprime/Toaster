6b4828af9ddc537c586bf3a5d5ad9c258c9acfae
Vinoth Ragavan <vinothprofessional@outlook.com>
2019-03-07 00:02:34 +0530

    Method name "message" changed to "msg" and added enable disable, duration control added.

--- toasterlib/src/main/java/com/vinmacro/toasterlib/Toaster.java
+++ toasterlib/src/main/java/com/vinmacro/toasterlib/Toaster.java
@@ -5,8 +5,15 @@ import android.widget.Toast;
 
 public class Toaster {
 
-    public static  void message(Context c,String msg){
 
-        Toast.makeText(c,msg,Toast.LENGTH_SHORT).show();
+    public static void msg(Context c, String msg, boolean isLongDuration, boolean isHavetoShow) {
+
+        if (isHavetoShow) {
+
+            if (isLongDuration)
+                Toast.makeText(c, msg, Toast.LENGTH_LONG).show();
+            else
+                Toast.makeText(c, msg, Toast.LENGTH_SHORT).show();
+        }
     }
 }
